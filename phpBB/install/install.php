8b33742deb58b38b0a45d1331c91675bdf88689c
Emmanuel Bourg <ebourg@apache.org>
2018-03-13 17:45:43 +0100

    Removed the calls to the magic quotes functions

--- phpBB/install/install.php
+++ phpBB/install/install.php
@@ -223,7 +223,6 @@ function guess_lang()
 
 // Begin
 error_reporting  (E_ERROR | E_WARNING | E_PARSE); // This will NOT report uninitialized variables
-set_magic_quotes_runtime(0); // Disable magic_quotes_runtime
 
 // PHP5 with register_long_arrays off?
 if (!isset($HTTP_POST_VARS) && isset($_POST))
@@ -243,7 +242,7 @@ if (!isset($HTTP_POST_VARS) && isset($_POST))
 }
 
 // Slash data if it isn't slashed
-if (!get_magic_quotes_gpc())
+if (true)
 {
 	if (is_array($HTTP_GET_VARS))
 	{
