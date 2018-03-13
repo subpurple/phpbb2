9b18402740c8304f1643df5c5eef472f9e767203
Emmanuel Bourg <ebourg@apache.org>
2018-03-13 23:40:49 +0100

    Fixed the warnings "Constant IN_PHPBB already defined"

--- phpBB/admin/admin_smilies.php
+++ phpBB/admin/admin_smilies.php
@@ -23,7 +23,10 @@
 *	This file will be used for modifying the smiley settings for a board.
 **************************************************************************/
 
-define('IN_PHPBB', 1);
+if (!defined('IN_PHPBB'))
+{
+    define( 'IN_PHPBB', 1);
+}
 
 //
 // First we do the setmodules stuff for the admin cp.
